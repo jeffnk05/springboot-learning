b5ad03c66bed24097f58daf622c784b9bf078576
Jeff <j.edjekoomhene@gmail.com>
2022-09-26 07:22:31 +0200

    Added model and repository

---
+++ src/main/java/com/jeff/learningspring/util/AppStartupEvent.java
@@ -0,0 +1,23 @@
+package com.jeff.learningspring.util;
+
+import com.jeff.learningspring.data.Room;
+import com.jeff.learningspring.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
